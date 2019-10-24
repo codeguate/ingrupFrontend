bb95a953530cd81a0778f44ccb43985b3ce5473e
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-10-24 11:53:53 -0600

    [UPDATED] added news

--- src/app/layout/components/tables-prods/tables-prods.component.ts
+++ src/app/layout/components/tables-prods/tables-prods.component.ts
@@ -14,6 +14,7 @@ export class TablesProdsComponent implements OnInit {
       private TablesComponent:FormComponent
   ) { }
   selected:any = {
+      id: 0,
       descripcion: [],
       peso: [],
       largo: [],
@@ -34,71 +35,19 @@ export class TablesProdsComponent implements OnInit {
 
   ngOnInit() {
     if(this.selectedData.presentaciones){
-        this.selected.descripcion = [
-          this.selectedData.presentaciones[0]
-        ]
-        let sinRepetidos = this.selectedData.presentaciones.filter((valorActual, indiceActual, arreglo) => {
-            //Podríamos omitir el return y hacerlo en una línea, pero se vería menos legible
-            return arreglo.findIndex(valorDelArreglo => (valorDelArreglo.nombre)==(valorActual.nombre)) === indiceActual
-        });
-        this.complete.descripcion = sinRepetidos
-
-        this.selected.peso = [
-          this.selectedData.presentaciones[0]
-        ]
-        sinRepetidos = this.selectedData.presentaciones.filter((valorActual, indiceActual, arreglo) => {
-            //Podríamos omitir el return y hacerlo en una línea, pero se vería menos legible
-            return arreglo.findIndex(valorDelArreglo => (valorDelArreglo.peso)==(valorActual.peso)) === indiceActual
-        });
-        this.complete.peso = sinRepetidos
-        this.selected.largo = [
-          this.selectedData.presentaciones[0]
-        ]
-        sinRepetidos = this.selectedData.presentaciones.filter((valorActual, indiceActual, arreglo) => {
-            //Podríamos omitir el return y hacerlo en una línea, pero se vería menos legible
-            return arreglo.findIndex(valorDelArreglo => (valorDelArreglo.largo)==(valorActual.largo)) === indiceActual
-        });
-        this.complete.largo = sinRepetidos
-        this.selected.cuello = [
-          this.selectedData.presentaciones[0]
-        ]
-        sinRepetidos = this.selectedData.presentaciones.filter((valorActual, indiceActual, arreglo) => {
-            //Podríamos omitir el return y hacerlo en una línea, pero se vería menos legible
-            return arreglo.findIndex(valorDelArreglo => (valorDelArreglo.cuello)==(valorActual.cuello)) === indiceActual
-        });
-        this.complete.cuello = sinRepetidos
-        this.selected.altura = [
-          this.selectedData.presentaciones[0]
-        ]
-        sinRepetidos = this.selectedData.presentaciones.filter((valorActual, indiceActual, arreglo) => {
-            //Podríamos omitir el return y hacerlo en una línea, pero se vería menos legible
-            return arreglo.findIndex(valorDelArreglo => (valorDelArreglo.altura)==(valorActual.altura)) === indiceActual
-        });
-        this.complete.altura = sinRepetidos
-        this.selected.unidades = [
-          this.selectedData.presentaciones[0]
-        ]
-        sinRepetidos = this.selectedData.presentaciones.filter((valorActual, indiceActual, arreglo) => {
-            //Podríamos omitir el return y hacerlo en una línea, pero se vería menos legible
-            return arreglo.findIndex(valorDelArreglo => (valorDelArreglo.unidades)==(valorActual.unidades)) === indiceActual
-        });
-        this.complete.unidades = sinRepetidos
-        this.selected.separador = [
-          this.selectedData.presentaciones[0]
-        ]
-        sinRepetidos = this.selectedData.presentaciones.filter((valorActual, indiceActual, arreglo) => {
-            //Podríamos omitir el return y hacerlo en una línea, pero se vería menos legible
-            return arreglo.findIndex(valorDelArreglo => (valorDelArreglo.separador)==(valorActual.separador)) === indiceActual
-        });
-        this.complete.separador = sinRepetidos
+        this.selected = this.selectedData.presentaciones[0]
       }
   }
   onChange($event){
     //   this.complete=$event
     let data = $event
-    this.selected =data
+    this.selected = data
     //   console.log(data);
+    //   console.log(this.selectedData.presentaciones[0]);
 
+  }
+  onClear(){
+    this.selected = this.selectedData.presentaciones[0]
   }
   removeDuplicates(originalArray, prop) {
 
