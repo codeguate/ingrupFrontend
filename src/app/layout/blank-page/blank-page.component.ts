52e5c25c46368ffc872f442cbf03079b7721e492
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-10-31 10:18:38 -0600

    [UPDATED] added cotizar funcional

--- src/app/layout/blank-page/blank-page.component.ts
+++ src/app/layout/blank-page/blank-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import {NgbAccordionConfig} from '@ng-bootstrap/ng-bootstrap';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { MarcasService } from "./../../shared/services/marcas.service";
+import { ActivatedRoute } from '@angular/router';
 declare var $: any
 @Component({
     selector: 'app-blank-page',
@@ -10,22 +13,25 @@ declare var $: any
 })
 export class BlankPageComponent implements OnInit {
     @BlockUI() blockUI: NgBlockUI;
-    constructor(config: NgbAccordionConfig) {
+    constructor(
+        config: NgbAccordionConfig,
+        private route: ActivatedRoute,
+        public router: Router,
+        private mainService:MarcasService
+    ) {
         // customize default values of accordions used by this component tree
         config.closeOthers = true;
         config.type = 'success';
       }
-
+    id = this.route.snapshot.paramMap.get("marca")
+    Table=[]
     ngOnInit() {
-        this.blockUI.start();
-        const data = {
-            id:1,
-            state:'0',
-            filter:'evento'
-        };
-        setTimeout(() => {
-            this.blockUI.stop();
-        }, 2000);
+        // this.blockUI.start();
+
+        // setTimeout(() => {
+        //     this.blockUI.stop();
+        // }, 2000);
+        this.cargarAll()
         //this.blockUI.stop();
     }
     button_mapa(){
@@ -34,5 +40,39 @@ export class BlankPageComponent implements OnInit {
     status: boolean = false;
     clickEvent(){
         this.status = !this.status;
+    }
+    cargarAll(){
+        this.blockUI.start();
+            let data = {
+                data:0,
+                id:0,
+                filter:"hijos"
+            }
+          this.mainService.getAllFilter(data)
+                              .then(response => {
+                                  this.id = this.route.snapshot.paramMap.get("marca")
+                                //   console.log(this.id);
+
+                                  response.forEach(element => {
+                                      if(element.id==this.id){
+                                        element.foto = element.foto.substring(0,element.foto.length-4)+"01.svg"
+                                        element.fotoActiva = element.foto
+                                        element.selected = true
+                                        // console.log(element);
+
+                                      }else{
+                                        element.selected = false
+
+                                      }
+                                  });
+                                //   let foto = response[response.findIndex(element => { element.id ==id })].foto
+                                // response[response.findIndex(element => { element.id ==id })].foto = foto.substring(0,foto.length-4)+"01.svg"
+                                this.Table=response
+                                this.blockUI.stop();
+
+                            }).catch(error => {
+                                this.blockUI.stop();
+                                console.clear
+                              })
     }
 }
--- src/app/layout/blank-page/blank-page.module.ts
+++ src/app/layout/blank-page/blank-page.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { BlankPageRoutingModule } from './blank-page-routing.module';
 import { BlankPageComponent } from './blank-page.component';
 
 @NgModule({
-    imports: [CommonModule, BlankPageRoutingModule, NgbModule],
+    imports: [CommonModule, FormsModule, BlankPageRoutingModule, NgbModule],
     declarations: [BlankPageComponent]
 })
 export class BlankPageModule {}
